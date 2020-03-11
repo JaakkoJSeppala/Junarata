ee4c23bd1c53bfa26c298ccb70944bd2b35b2fe5
Jaakko Seppälä <jaakko.j.seppala@gmail.com>
2020-03-11 11:53:58 +0200

    Initial project version

--- juna.py
+++ juna.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Putkaposti Vuoristorata.
 
-This is a temporary script file.
+Pisin näkee aina junasta. Toiseksi pisin näkee junasta puolissa tapauksissa.
+On yhtä todennäköistä, että hän on pisimmän edessä tai takana. Kolmanneksi
+pisin näkee junasta joka kolmannessa tapauksessa jne.
 """
 
 from fractions import Fraction
-a = Fraction(0,1)
-for case in range(5,32):
-    a = Fraction(0,1)
-    for i in range(1,case):
-        a += Fraction(1,i)
-    print(str(case-1) + " " +str(a))
+a = Fraction(0, 1)
+for case in range(5, 32):
+    a = Fraction(0, 1)
+    for i in range(1, case):
+        a += Fraction(1, i)
+    print(str(case-1) + " " + str(a))
